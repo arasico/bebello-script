6731e5895f070d338501eade0461027ad5d13149
yaser <yaser.darzi@gmail.com>
2017-12-03 18:50:22 +0330

    update login api v1 and package.json

--- Controllers/Api/V1/AuthController.js
+++ Controllers/Api/V1/AuthController.js
@@ -1,25 +1,52 @@
-var UserModel = require('../../../models/user');
+var userModel = require('../../../models/user');
 var crypto = require('crypto');
 
 exports.postLogin = function (req, res) {
+
+
+    var user = userModel.findOne({email: req.body.email}, function (err, user) {
+        console.log(user);
+
+        // res.json(user);
+        // Project.findOne({name: project, user: user._id}, function(err, project){
+        //     if(err) {
+        //         console.log(err);
+        //         return
+        //     }
+        //
+        //     Issues.find({project_id: project._id}, function(err, issues){
+        //         if(err) {
+        //             console.log(err);
+        //             return
+        //         }
+        //
+        //         res.render('./views/issues/index', {user: user, project: poject, issues: issues});
+        //     })
+        // });
+    });
+
+    res.json(req.params.email);
+
+    // UserModel   .findOne({_id: "5a1e5099de6d162b64074c72"}, function(err, document) {
+    // });
+
+    // UserModel.findOne({'email': 'info'}, function (err, usr) {
+    //     if (err) throw err;
+    //     res.json(usr);
+    // })
+
+
     res.send('NOT IMPLEMENTED: login');
+
 };
 
 exports.postRegister = function (req, res) {
-
-    // User.findOne({email: req.body.email}, function (err, user) {
-    //     if (err) throw err;
-    //     // res.json(user);
-    // });
-
-
     var secret = req.header('uuid');
     var hash = crypto.createHmac('sha256', secret)
         .update(req.body.email)
         .digest('hex');
-
     // create a user a new user
-    var user = new UserModel({
+    var user = new userModel({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
@@ -34,12 +61,9 @@ exports.postRegister = function (req, res) {
             updated_at: Date.now()
         }
     });
-
-
     user.save(function (err) {
         if (err) throw err;
     });
-
     res.json(user);
 };
 
